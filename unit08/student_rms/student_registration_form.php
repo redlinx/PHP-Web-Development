9c9590bcf569f9a054f8c09959ed24a633d30c5b
Francis Rey Padao <francisreypadao@gmail.com>
2018-11-23 15:54:47 +0800

    no message

--- unit08/student_rms/student_registration_form.php
+++ unit08/student_rms/student_registration_form.php
@@ -1,40 +1,42 @@
 <h2>Student Registration Form</h2>
-
+<a href="home.php">Home</a>
+<br>
+<br>
 <table>
 
 <form method="POST" action="insert_student.php">
 	<tr>
-		<td>Last Name</td>
-		<td><input type="text" name="stud_lname" value=""></td>
+		<!-- <td>Last Name</td> -->
+		<td><input type="text" name="stud_lname" value="" placeholder="Last name" required></td>
 	</tr>
 	
 	<tr>
-		<td>First Name</td>
-		<td><input type="text" name="stud_fname" value=""></td>
+		<!-- <td>First Name</td> -->
+		<td><input type="text" name="stud_fname" value="" placeholder="First name" required></td>
 	</tr>
 	
 	<tr>
-		<td>Middle Name</td>
-		<td><input type="text" name="stud_mname" value=""></td>
+		<!-- <td>Middle Name</td> -->
+		<td><input type="text" name="stud_mname" value="" placeholder="Middle Name" required></td>
 	</tr>
 
 	<tr>
-		<td>Email Address</td>
-		<td><input type="text" name="stud_email" value=""></td>
+		<!-- <td>Email Address</td> -->
+		<td><input type="email" name="stud_email" value="" placeholder="Email Address" pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}$" required></td>
 	</tr>
 
 	<tr>
-		<td>Mobile Number</td>
-		<td><input type="text" name="stud_mob_no" value=""></td>
+		<!-- <td>Mobile Number</td> -->
+		<td><input type="text" name="stud_mob_no" value="" placeholder="Mobile Number" pattern="[0-9]+" required></td>
 	</tr>
 
 	<tr>
-		<td>Birthday</td>
-		<td><input type="date" name="stud_bday" value=""></td>
+		<!-- <td>Birthday</td> -->
+		<td><input type="date" name="stud_bday" value="" required></td>
 	</tr>
 
 	<tr>
-		<td colspan=2><center><input type="submit" name="submit" value="save"></center></td>
+		<td><input type="submit" name="submit" value="save"></td>
 	</tr>
 </form>
 
